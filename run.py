47e29f6238e7a1af7fc406f22a3c9a2b240a0f7e
Simon Gordon <gordonsimon@hotmail.com>
2017-07-29 18:51:40 +1000

    separating app into folder

--- run.py
+++ run.py
@@ -1,11 +1,3 @@
 
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-    return "Hello, World!"
-
-if __name__ == "__main__":
-    app.run()
+from app import app
+app.run(host='0.0.0.0', port=8088, debug=True)
