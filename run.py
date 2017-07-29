559c55ad41a6f5872b0c56d017468a3473761f22
Simon Gordon <gordonsimon@hotmail.com>
2017-07-29 18:42:59 +1000

    Hello world

--- run.py
+++ run.py
@@ -1,2 +1,11 @@
 
-print "Application running"
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello, World!"
+
+if __name__ == "__main__":
+    app.run()
