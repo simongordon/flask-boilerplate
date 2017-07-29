906ed9ddfe41a0aed64519df87ce39a81f39a7b5
Simon Gordon <gordonsimon@hotmail.com>
2017-07-29 22:15:46 +1000

    Learning jinja

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,16 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.template_filter()
+def meme(value):
+    return 'meme ' + value + ' meme'
+#app.jinja_env.filters['meme'] = meme
+
 @app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template('index.html', title='Home')
 
+@app.route("/other")
+def other():
+    my_list=[1,2,3]
+    return render_template('other.html', title='Other Page', my_list=my_list)
