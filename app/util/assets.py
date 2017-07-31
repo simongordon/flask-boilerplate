a7d3762282cd9bae3b18ea20c5334c7062a3cb01
Simon Gordon <gordonsimon@hotmail.com>
2017-07-31 14:08:14 +1000

    working on generated assets

--- app/util/assets.py
+++ app/util/assets.py
@@ -3,14 +3,21 @@ from flask_assets import Bundle, Environment
 from .. import app
 bundles = {
         'home_js': Bundle(
-            #'bower_components/jquery/dist/jquery.js',
             'bower_components/jquery/dist/jquery.min.js',
             output='gen/home.js'),
+        #'home_css': Bundle(
+            #output='gen/home.css'),
         'react_js': Bundle(
             'bower_components/babel-standalone/babel.min.js',
             'bower_components/react/react.min.js',
             'bower_components/react/react-dom.min.js',
-            output='gen/react.js')
+            output='gen/react.js'),
+        'bootstrap_js': Bundle(
+            'bower_components/bootstrap/dist/js/bootstrap.min.js',
+            output='gen/bootstrap.js'),
+        'bootstrap_css': Bundle(
+            'bower_components/bootstrap/dist/css/bootstrap.min.css',
+            output='gen/bootstrap.css'),
         }
 
 assets = Environment(app)
