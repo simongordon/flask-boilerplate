146b8374b4bba119e6dc78671ee3ec9b8d7830c0
Simon Gordon <gordonsimon@hotmail.com>
2017-07-31 13:48:03 +1000

    playing with react stuff

--- app/util/assets.py
+++ app/util/assets.py
@@ -5,7 +5,12 @@ bundles = {
         'home_js': Bundle(
             #'bower_components/jquery/dist/jquery.js',
             'bower_components/jquery/dist/jquery.min.js',
-            output='gen/home.js')
+            output='gen/home.js'),
+        'react_js': Bundle(
+            'bower_components/babel-standalone/babel.min.js',
+            'bower_components/react/react.min.js',
+            'bower_components/react/react-dom.min.js',
+            output='gen/react.js')
         }
 
 assets = Environment(app)
